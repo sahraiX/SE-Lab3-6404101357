ce7e22879e4ec7387e8d096d7f0276b3ef6d87a2
sahraiX <mju6404101357@mju.ac.th>
2023-12-15 10:16:14 +0700

    changed name from eng to thai

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -9,10 +9,10 @@
       <q-input
         filled
         v-model="name"
-        label="Your name *"
-        hint="Name and surname"
+        label="ชื่อ-สกุล *"
+        hint="ใส่ชื่อและนามสกุล"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
+        :rules="[ val => val && val.length > 0 || 'กรุณาใส่ชื่อและนามสกุลด้วยจ้า']"
       />
 
       <q-input
